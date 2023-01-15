db119ded752df04ef59745b92de6345512751dee
Isaac1011 <sandoval.5im6.v4@gmail.com>
2023-01-15 00:06:32 -0600

    Interfaz_TotalPagar_Carrito

--- client/src/views/CartView.vue
+++ client/src/views/CartView.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="bg-white">
       <div class="mx-auto max-w-2xl px-4 pt-16 pb-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Shopping Cart</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-main-color sm:text-4xl">Productos añadidos al carrito</h1>
         <form class="mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16">
           <section aria-labelledby="cart-heading" class="lg:col-span-7">
-            <h2 id="cart-heading" class="sr-only">Items in your shopping cart</h2>
+            <h2 id="cart-heading" class="sr-only">Items in your shopping </h2>
   
             <ul role="list" class="divide-y divide-gray-200 border-t border-b border-gray-200">
-              <li v-for="(product, productIdx) in products" :key="product.id" class="flex py-6 sm:py-10">
+              <li v-for="(product) in products" :key="product.id" class="flex py-6 sm:py-10">
                 <div class="flex-shrink-0">
                   <img :src="product.imageSrc" :alt="product.imageAlt" class="h-24 w-24 rounded-md object-cover object-center sm:h-48 sm:w-48" />
                 </div>
@@ -21,39 +21,19 @@
                         </h3>
                       </div>
                       <div class="mt-1 flex text-sm">
-                        <p class="text-gray-500">{{ product.color }}</p>
-                        <p v-if="product.size" class="ml-4 border-l border-gray-200 pl-4 text-gray-500">{{ product.size }}</p>
+                        <p class="text-gray-500">{{ product.price }}</p>
+                        <p class="ml-4 border-l border-gray-200 pl-4 text-gray-500">{{ product.store_name }}</p>
                       </div>
-                      <p class="mt-1 text-sm font-medium text-gray-900">{{ product.price }}</p>
-                    </div>
-  
-                    <div class="mt-4 sm:mt-0 sm:pr-9">
-                      <label :for="`quantity-${productIdx}`" class="sr-only">Quantity, {{ product.name }}</label>
-                      <select :id="`quantity-${productIdx}`" :name="`quantity-${productIdx}`" class="max-w-full rounded-md border border-gray-300 py-1.5 text-left text-base font-medium leading-5 text-gray-700 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                      </select>
-  
-                      <div class="absolute top-0 right-0">
-                        <button type="button" class="-m-2 inline-flex p-2 text-gray-400 hover:text-gray-500">
-                          <span class="sr-only">Remove</span>
-                          <XMarkIcon class="h-5 w-5" aria-hidden="true" />
-                        </button>
-                      </div>
+                      <!--p class="mt-1 text-sm font-medium text-gray-900">{{ product.price }}</p-->
                     </div>
                   </div>
   
-                  <p class="mt-4 flex space-x-2 text-sm text-gray-700">
+                  <!--p class="mt-4 flex space-x-2 text-sm text-gray-700">
                     <CheckIcon v-if="product.inStock" class="h-5 w-5 flex-shrink-0 text-green-500" aria-hidden="true" />
                     <ClockIcon v-else class="h-5 w-5 flex-shrink-0 text-gray-300" aria-hidden="true" />
                     <span>{{ product.inStock ? 'In stock' : `Ships in ${product.leadTime}` }}</span>
-                  </p>
+                  </p-->
+
                 </div>
               </li>
             </ul>
@@ -61,84 +41,89 @@
   
           <!-- Order summary -->
           <section aria-labelledby="summary-heading" class="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
-            <h2 id="summary-heading" class="text-lg font-medium text-gray-900">Order summary</h2>
+            <h2 id="summary-heading" class="text-lg font-medium text-secondary-color">Resumen de la orden</h2>
   
             <dl class="mt-6 space-y-4">
               <div class="flex items-center justify-between">
-                <dt class="text-sm text-gray-600">Subtotal</dt>
-                <dd class="text-sm font-medium text-gray-900">$99.00</dd>
-              </div>
-              <div class="flex items-center justify-between border-t border-gray-200 pt-4">
-                <dt class="flex items-center text-sm text-gray-600">
-                  <span>Shipping estimate</span>
-                  <a href="#" class="ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500">
-                    <span class="sr-only">Learn more about how shipping is calculated</span>
-                    <QuestionMarkCircleIcon class="h-5 w-5" aria-hidden="true" />
-                  </a>
-                </dt>
-                <dd class="text-sm font-medium text-gray-900">$5.00</dd>
-              </div>
-              <div class="flex items-center justify-between border-t border-gray-200 pt-4">
-                <dt class="flex text-sm text-gray-600">
-                  <span>Tax estimate</span>
-                  <a href="#" class="ml-2 flex-shrink-0 text-gray-400 hover:text-gray-500">
-                    <span class="sr-only">Learn more about how tax is calculated</span>
-                    <QuestionMarkCircleIcon class="h-5 w-5" aria-hidden="true" />
-                  </a>
-                </dt>
-                <dd class="text-sm font-medium text-gray-900">$8.32</dd>
-              </div>
-              <div class="flex items-center justify-between border-t border-gray-200 pt-4">
-                <dt class="text-base font-medium text-gray-900">Order total</dt>
-                <dd class="text-base font-medium text-gray-900">$112.32</dd>
+                <dt class="text-sm text-gray-600">Total a pagar</dt>
+
+
+                <dd class="text-sm font-medium text-gray-900">{{totalReactiva}}</dd>
               </div>
             </dl>
   
             <div class="mt-6">
-              <button type="submit" class="w-full rounded-md border border-transparent bg-indigo-600 py-3 px-4 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">Checkout</button>
+              <button type="submit" class="w-full rounded-md border border-main-color bg-main-color py-3 px-4 text-base font-medium text-secondary-color shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50">Pagar</button>
             </div>
           </section>
         </form>
       </div>
     </div>
   </template>
-  
-  <script setup lang="ts">
-  import { CheckIcon, ClockIcon, QuestionMarkCircleIcon, XMarkIcon } from '@heroicons/vue/20/solid'
-  
-  const products = [
+
+
+
+
+
+
+
+<script setup lang="ts">
+  //import { CheckIcon, ClockIcon, QuestionMarkCircleIcon, XMarkIcon } from '@heroicons/vue/20/solid'
+import Item from "../components/items/Item.vue"
+import { ref, onMounted } from "vue";
+
+const totalReactiva = ref(0)
+
+const calcula = () => {
+  products.forEach(product => totalReactiva.value += product.price2 )
+}
+
+onMounted( () => {
+  calcula()
+})
+
+
+const products = [
     {
       id: 1,
-      name: 'Basic Tee',
+      name: 'Chilaquiles',
       href: '#',
-      price: '$32.00',
-      color: 'Sienna',
-      inStock: true,
-      size: 'Large',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg',
-      imageAlt: "Front of men's Basic Tee in sienna.",
+      imageSrc: 'https://www.clara.es/medio/2021/12/16/que-comer-hoy-ideas_21beeb02_1200x630.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$35',
+      price2: 35,
+      store_name: 'Juanitas',
+    },
+    {
+      id: 1,
+      name: 'Pasta',
+      href: '#',
+      imageSrc: 'https://www.cocinacaserayfacil.net/wp-content/uploads/2020/04/Recetas-de-comidas-para-ni%C3%B1os.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$45',
+      price2: 45,
+      store_name: 'Santiagos',
     },
     {
       id: 2,
-      name: 'Basic Tee',
+      name: 'Torta',
       href: '#',
-      price: '$32.00',
-      color: 'Black',
-      inStock: false,
-      leadTime: '3–4 weeks',
-      size: 'Large',
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-02.jpg',
+      imageSrc: 'https://www.cocinacaserayfacil.net/wp-content/uploads/2020/04/Recetas-de-comidas-para-ni%C3%B1os.jpg',
       imageAlt: "Front of men's Basic Tee in black.",
+      price: '$85',
+      price2: 85,
+      store_name: 'Miguel',
     },
     {
       id: 3,
-      name: 'Nomad Tumbler',
+      name: 'Sincronizada',
       href: '#',
-      price: '$35.00',
-      color: 'White',
-      inStock: true,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-03.jpg',
-      imageAlt: 'Insulated bottle with white base and black snap lid.',
+      imageSrc: 'https://www.cocinacaserayfacil.net/wp-content/uploads/2020/04/Recetas-de-comidas-para-ni%C3%B1os.jpg',
+      imageAlt: "Front of men's Basic Tee in black.",
+      price: '$20',
+      price2: 35,
+      store_name: 'Isaac',
     },
   ]
   </script>
+
