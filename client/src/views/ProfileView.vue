efa56aee139c8b857f2bddb7e8ccf84eac6dab64
MarcoLg23 <marco@MLG-Vivobook>
2023-01-15 21:18:16 -0600

    ya con profileview pero no conectado a base de datos

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,98 +1,103 @@
-<template>
-    <div>
-      <main>
-        <div class="mx-auto max-w-screen-xl px-4 pb-6 sm:px-6 lg:px-8 lg:pb-16">
-          <div class="overflow-hidden rounded-lg bg-white shadow">
-            <div class="divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-y-0 lg:divide-x">
-              <aside class="py-6 lg:col-span-3">
-                <nav class="space-y-1">
-                  <a v-for="item in subNavigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-teal-50 border-teal-500 text-teal-700 hover:bg-teal-50 hover:text-teal-700' : 'border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900', 'group border-l-4 px-3 py-2 flex items-center text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">
-                    <component :is="item.icon" :class="[item.current ? 'text-teal-500 group-hover:text-teal-500' : 'text-gray-400 group-hover:text-gray-500', 'flex-shrink-0 -ml-1 mr-3 h-6 w-6']" aria-hidden="true" />
-                    <span class="truncate">{{ item.name }}</span>
-                  </a>
-                </nav>
-              </aside>
-  
-              <form class="divide-gray-200 lg:col-span-9" action="#" method="POST">
-                <!-- Profile section -->
-                <div class="py-6 px-4 sm:p-6 lg:pb-8">
-                  <div>
-                    <h2 class="text-lg font-medium leading-6 text-gray-900">Perfil</h2>
-                    <p>{{ userInfo.name }}</p>
-                  </div>
-  
-                  <div class="mt-6 flex flex-col lg:flex-row">
-                    <div class="flex-grow space-y-6">
-                      <div>
-                        <label for="username" class="block text-sm font-medium text-gray-700">Usuario</label>
-                        <div class="mt-1 flex rounded-md shadow-sm">
-                          <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-sky-500 focus:ring-sky-500 sm:text-sm" :value="userInfo.handle" />
-                        </div>
-                      </div>
-  
-                      <div>
-                        <label for="about" class="block text-sm font-medium text-gray-700">Descripción</label>
-                        <div class="mt-1">
-                          <textarea id="about" name="about" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm" />
-                        </div>
+<template v-if="dataReady">
+  <div>
+    <main>
+      <div class="mx-auto max-w-screen-xl px-4 pb-6 sm:px-6 lg:px-8 lg:pb-16">
+        <div class="overflow-hidden rounded-lg bg-white shadow">
+          <div class="divide-y divide-gray-200 lg:grid lg:grid-cols-12 lg:divide-y-0 lg:divide-x">
+            <aside class="py-6 lg:col-span-3">
+              <nav class="space-y-1">
+                <a v-for="item in subNavigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-teal-50 border-teal-500 text-teal-700 hover:bg-teal-50 hover:text-teal-700' : 'border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900', 'group border-l-4 px-3 py-2 flex items-center text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">
+                  <component :is="item.icon" :class="[item.current ? 'text-teal-500 group-hover:text-teal-500' : 'text-gray-400 group-hover:text-gray-500', 'flex-shrink-0 -ml-1 mr-3 h-6 w-6']" aria-hidden="true" />
+                  <span class="truncate">{{ item.name }}</span>
+                </a>
+              </nav>
+            </aside>
+
+            <form class="divide-gray-200 lg:col-span-9" action="#" method="POST">
+              <!-- Profile section -->
+              <div class="py-6 px-4 sm:p-6 lg:pb-8">
+                <div>
+                  <h2 class="text-lg font-medium leading-6 text-gray-900">Perfil</h2>
+                  <p>{{ userInfo.email }}</p>
+                </div>
+
+                <div class="mt-6 flex flex-col lg:flex-row">
+                  <div class="flex-grow space-y-6">
+                    <div>
+                      <label for="username" class="block text-sm font-medium text-gray-700">Usuario</label>
+                      <div class="mt-1 flex rounded-md shadow-sm">
+                        <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-sky-500 focus:ring-sky-500 sm:text-sm" :value="userInfo.handle" />
                       </div>
                     </div>
-  
-                    <div class="mt-6 flex-grow lg:mt-0 lg:ml-6 lg:flex-shrink-0 lg:flex-grow-0">
-                      <p class="text-sm font-medium text-gray-700" aria-hidden="true">Foto</p>
-                      <div class="mt-1 lg:hidden">
-                        <div class="flex items-center">
-                          <div class="inline-block h-12 w-12 flex-shrink-0 overflow-hidden rounded-full" aria-hidden="true">
-                            <img class="h-full w-full rounded-full" :src="user.imageUrl" alt="" />
-                          </div>
-                          <div class="ml-5 rounded-md shadow-sm">
-                            <div class="group relative flex items-center justify-center rounded-md border border-gray-300 py-2 px-3 focus-within:ring-2 focus-within:ring-sky-500 focus-within:ring-offset-2 hover:bg-gray-50">
-                              <label for="mobile-user-photo" class="pointer-events-none relative text-sm font-medium leading-4 text-gray-700">
-                                <span>Cambiar</span>
-                                <span class="sr-only"> foto de usuario</span>
-                              </label>
-                              <input id="mobile-user-photo" name="user-photo" type="file" class="absolute h-full w-full cursor-pointer rounded-md border-gray-300 opacity-0" />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-  
-                      <div class="relative hidden overflow-hidden rounded-full lg:block">
-                        <img class="relative h-40 w-40 rounded-full" :src="user.imageUrl" alt="" />
-                        <label for="desktop-user-photo" class="absolute inset-0 flex h-full w-full items-center justify-center bg-black bg-opacity-75 text-sm font-medium text-white opacity-0 focus-within:opacity-100 hover:opacity-100">
-                          <span>Cambiar</span>
-                          <span class="sr-only"> foto de usuario</span>
-                          <input type="file" id="desktop-user-photo" name="user-photo" class="absolute inset-0 h-full w-full cursor-pointer rounded-md border-gray-300 opacity-0" />
-                        </label>
+
+                    <div>
+                      <label for="about" class="block text-sm font-medium text-gray-700">Descripción</label>
+                      <div class="mt-1">
+                        <textarea id="about" name="about" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-sky-500 focus:ring-sky-500 sm:text-sm" :value="userInfo.description" />
                       </div>
                     </div>
                   </div>
-  
-                  <div class="mt-6 grid grid-cols-12 gap-6">
-                    <div class="col-span-12 sm:col-span-6">
-                      <label for="first-name" class="block text-sm font-medium text-gray-700">Nombres</label>
-                      <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" :value="userInfo.name" />
+
+                  <div class="mt-6 flex-grow lg:mt-0 lg:ml-6 lg:flex-shrink-0 lg:flex-grow-0">
+                    <p class="text-sm font-medium text-gray-700" aria-hidden="true">Foto</p>
+                    <div class="mt-1 lg:hidden">
+                      <div class="flex items-center">
+                        <div class="inline-block h-12 w-12 flex-shrink-0 overflow-hidden rounded-full" aria-hidden="true">
+                          <img class="h-full w-full rounded-full" :src="userInfo.imageUrl" alt="" />
+                        </div>
+                        <div class="ml-5 rounded-md shadow-sm">
+                          <div class="group relative flex items-center justify-center rounded-md border border-gray-300 py-2 px-3 focus-within:ring-2 focus-within:ring-sky-500 focus-within:ring-offset-2 hover:bg-gray-50">
+                            <label for="mobile-user-photo" class="pointer-events-none relative text-sm font-medium leading-4 text-gray-700">
+                              <span>Cambiar</span>
+                              <span class="sr-only"> foto de usuario</span>
+                            </label>
+                            <input id="mobile-user-photo" name="user-photo" type="file" class="absolute h-full w-full cursor-pointer rounded-md border-gray-300 opacity-0" />
+                          </div>
+                        </div>
+                      </div>
                     </div>
-  
-                    <div class="col-span-12 sm:col-span-6">
-                      <label for="last-name" class="block text-sm font-medium text-gray-700">Apellidos</label>
-                      <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" :value="userInfo.lastname" />
-                    </div>
-  
-                    <div class="col-span-12 sm:col-span-6">
-                      <label for="company" class="block text-sm font-medium text-gray-700">Teléfono</label>
-                      <input type="text" name="company" id="company" autocomplete="organization" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" :value="userInfo.phone" />
+
+                    <div class="relative hidden overflow-hidden rounded-full lg:block">
+                      <img class="relative h-40 w-40 rounded-full" :src="userInfo.imageUrl" alt="" />
+                      <label for="desktop-user-photo" class="absolute inset-0 flex h-full w-full items-center justify-center bg-black bg-opacity-75 text-sm font-medium text-white opacity-0 focus-within:opacity-100 hover:opacity-100">
+                        <span>Cambiar</span>
+                        <span class="sr-only"> foto de usuario</span>
+                        <input type="file" id="desktop-user-photo" name="user-photo" class="absolute inset-0 h-full w-full cursor-pointer rounded-md border-gray-300 opacity-0" />
+                      </label>
                     </div>
                   </div>
                 </div>
-              </form>
-            </div>
+
+                <div class="mt-6 grid grid-cols-12 gap-6">
+                  <div class="col-span-12 sm:col-span-6">
+                    <label for="first-name" class="block text-sm font-medium text-gray-700">Nombres</label>
+                    <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" :value="userInfo.name" />
+                  </div>
+
+                  <div class="col-span-12 sm:col-span-6">
+                    <label for="last-name" class="block text-sm font-medium text-gray-700">Apellidos</label>
+                    <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" :value="userInfo.lastname" />
+                  </div>
+
+                  <div class="col-span-12 sm:col-span-6">
+                    <label for="company" class="block text-sm font-medium text-gray-700">Teléfono</label>
+                    <input type="text" name="company" id="company" autocomplete="organization" class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" :value="userInfo.phone" />
+                  </div>
+                  <div class="grow flex items-end">
+                      <a class="block font-medium text-white bg-red rounded-lg  px-4 py-2 text-center mt-auto max-w-[16rem] ml-auto"
+                          href="#"> GUARDAR
+                      </a>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-      </main>
-    </div>
-  </template>
-  
+      </div>
+    </main>
+  </div>
+</template>
+
   <script setup lang="ts">
   import { ref } from 'vue'
   import { supabase } from "../supabase"
@@ -108,21 +113,20 @@
     XMarkIcon,
   } from '@heroicons/vue/24/outline'
 
-  const { data: { user } } = await supabase.auth.getUser()
-  console.log(user.email);
 
-const userInfo = {
-    name: user.email,
-    lastname: 'Lavarrios',
-    handle: 'MarcoLg',
-    phone: '12345678',
-    email: 'marcolg@example.com',
-    imageUrl:
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F5692874%2Favatar_coding_computer_it_laptop_profile_programmer_icon&psig=AOvVaw3af4RvP2JCCqiM7vjvjl8d&ust=1673909327057000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPCCv6rUyvwCFQAAAAAdAAAAABAE',
+  const userInfo = {
+      email: 'marcolg@test.com',
+      handle: 'MarcoLg',
+      description: 'Catador de tacos',
+      name: 'Marco',
+      lastname: 'Lavarrios',
+      phone: '12345678',
+      imageUrl:
+        'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=320&h=320&q=80',
   }
   const subNavigation = [
-    { name: 'Profile', href: '#/profile', icon: UserCircleIcon, current: true },
-    { name: 'Password', href: '#/password', icon: KeyIcon, current: false },
+    { name: 'Profile', href: '#', icon: UserCircleIcon, current: true },
+    { name: 'Password', href: '../password',  icon: KeyIcon, current: false },
   ]
-  
+
   </script>
