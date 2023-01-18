86b26c1ad60ce97c52195c3038f22ab007291562
mikemrg17 <miguelangelmurga1234@gmail.com>
2023-01-18 11:15:33 -0600

    Orders module

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -74,6 +74,21 @@ const router = createRouter({
             path:"/stores/:id",
             name: "store",
             component: () =>  import("../views/StoreView.vue"),
+        },
+        {
+            path:"/newProduct",
+            name: "newProduct",
+            component: () =>  import("../views/NewProductView.vue"),
+        },
+        {
+            path:"/newStore",
+            name: "newStore",
+            component: () =>  import("../views/NewStoreView.vue"),
+        },
+        {
+            path: "/orders",
+            name: "orders",
+            component: () => import("../views/OrdersView.vue")
         }
     ],
 })
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,15 +1,28 @@
-<template>
+<!--<template>
     <main class="max-w-full">
-        <div class="h-screen text-center bg-white">
+        
+    </main>
+</template>-->
+<template>
+    <main class="bg-white max-w-full max-h-full">
+      <div class="mx-auto p-28 lg:px-8">
+        <div class="text-center">
+          <h2 class="text-lg font-semibold text-indigo-600">Bienvenido a</h2>
+          <p class="mt-1 text-4xl font-bold tracking-tight text-main-color sm:text-5xl lg:text-6xl">Escomidita</p>
+          <p class="mx-auto mt-5 max-w-xl text-xl text-black">Comienza a comprar comida a los compañeros de la ESCOMunidad</p>
+        </div>
+      </div>
+      <div class="text-center bg-white">
             <RouterLink to="/signUp">
-                <button class="h-10 rounded-[20px] cursor-pointer bg-black text-white">Registrarse</button>
+                <button class="h-10 rounded-[20px] cursor-pointer bg-black text-main-color">Registrarse</button>
             </RouterLink>
+            |
             <RouterLink to="/signIn">
-                <button class="h-10 rounded-[20px] cursor-pointer bg-black text-white">Iniciar sesión</button>
+                <button class="h-10 rounded-[20px] cursor-pointer bg-black text-secondary-color">Iniciar sesión</button>
             </RouterLink>
         </div>
     </main>
-</template>
+  </template>
 
 <script setup lang="ts">
 import { RouterLink } from "vue-router"
